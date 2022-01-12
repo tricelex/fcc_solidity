1271ab3aca271321fb09cc04778ae7e513ff2e9e
Remix Workspace <>
2022-01-12 07:22:18 +0100

    Storage FActory

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity  >=0.8.0 <0.9.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        
+    }
+}    
